4699becfaa902557c1b88c211b447cd9b832fd5c
Andrew Dobrik <omgdobrik@gmail.com>
2022-02-24 15:19:22 +0300

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,10 @@
+var myName = "Андрей";
+if (myName == "Андрей") {
+   console.log("Привет мне!");
+} else if (myName === "Сергей") {
+   console.log("Привет отец!");
+} else if (myName === "Ирина") {
+   console.log("Привет мама!");
+} else {
+   console.log("Привет незнакомец!");
+}
