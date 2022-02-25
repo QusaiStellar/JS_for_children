0400e66317a5a5ccbb33f2ee05f67afd6571f456
Andrew Dobrik <omgdobrik@gmail.com>
2022-02-25 16:12:24 +0300

    Update 16.js

--- 16.js
+++ 16.js
@@ -1,39 +1,35 @@
-var words = ["Слава Украине", "Жыве Беларусь", "птн пнх",];
+var words = ["Слава Украине!", "Жыве беларусь", "Птн пнх",];
 var randomWord = words[Math.floor(Math.random() * words.length)];
 var finalWord = [];
 for (i = 0; i < randomWord.length; i++) {
    finalWord[i] = "_";
 }
 var remainingLetters = randomWord.length;
-console.log(finalWord);
 while (remainingLetters > 0) {
-   alert(finalWord.join(" ") + " Осталось попыток: " + remainingLetters);
+   alert(`${finalWord.join(" ")} Осталось букв: ${remainingLetters}`);
    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
    if (guess === null) {
-      break;
    } else if (guess.length !== 1) {
-      if (guess === randomWord) {
-         alert("Вы ввели слово: " + guess);
+      if (guess.toLowerCase() === randomWord.toLowerCase()) {
+         alert(`Вы ввели слово: ${guess.toLowerCase()} и это правильный ответ. Поздравяем!`);
          break;
       } else {
          remainingLetters--;
-         alert("Вы ввели слово: " + guess + " но данно слово неверно!! Осталось попыток: " + remainingLetters);
-
+         alert(`Вы ввели слово: ${guess} но оно неверно!! Осталось букв: ${remainingLetters}`);
       }
    } else if (guess.length == 1) {
-
-
       for (j = 0; j < randomWord.length; j++) {
-         if (randomWord[j] === guess) {
+         if (randomWord[j].toLowerCase() === guess.toLowerCase()) {
             finalWord[j] = guess;
+            remainingLetters--;
          }
       }
-      remainingLetters--;
    } else {
       remainingLetters--;
    }
 }
-alert("Окончательный результат: " + finalWord.join(" ") + " Осталось попыток: " + remainingLetters);
-alert("Отлично! Было загадано слово " + randomWord);
+
+alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")} Осталось букв: ${remainingLetters}`);
+alert(`Отлично! Было загадано слово ${randomWord}`);
 
 console.log(finalWord);
