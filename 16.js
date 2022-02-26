9f8fc81be447fec18fec7d5cee149461728dbd4f
Andrew Dobrik <omgdobrik@gmail.com>
2022-02-26 10:35:00 +0300

    Update 16.js

--- 16.js
+++ 16.js
@@ -5,31 +5,44 @@ for (i = 0; i < randomWord.length; i++) {
    finalWord[i] = "_";
 }
 var remainingLetters = randomWord.length;
-while (remainingLetters > 0) {
-   alert(`${finalWord.join(" ")} Осталось букв: ${remainingLetters}`);
+var attempts = randomWord.length;
+guessTheLetter: while (remainingLetters > 0) {
+   alert(`${finalWord.join(" ")} Осталось букв: ${remainingLetters}. Осталось попыток ${attempts}`);
    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
    if (guess === null) {
-   } else if (guess.length !== 1) {
+      break;
+   } else if (guess === "") {
+      alert(`Пожалуйста, введите хоть что-то!`);
+   } else if (guess.length > 1) {
       if (guess.toLowerCase() === randomWord.toLowerCase()) {
          alert(`Вы ввели слово: ${guess.toLowerCase()} и это правильный ответ. Поздравяем!`);
          break;
       } else {
-         remainingLetters--;
-         alert(`Вы ввели слово: ${guess} но оно неверно!! Осталось букв: ${remainingLetters}`);
+         attempts--;
+         alert(`Вы ввели слово: ${guess} но оно неверно!! Осталось попыток: ${attempts}. Осталось букв: ${remainingLetters}`);
       }
    } else if (guess.length == 1) {
       for (j = 0; j < randomWord.length; j++) {
          if (randomWord[j].toLowerCase() === guess.toLowerCase()) {
-            finalWord[j] = guess;
-            remainingLetters--;
+            if (finalWord[j] === "_") {
+               finalWord[j] = guess;
+               remainingLetters--;
+            } else {
+               alert(`Вы уже угадали букву: ${guess}`);
+               continue guessTheLetter;
+            }
          }
       }
-   } else {
-      remainingLetters--;
+      attempts--;
    }
+   if (attempts == false) {
+      alert(`Увы, но вы исчерпали все свои попытки.`);
+      break;
+   }
+   console.log(guess);
 }
 
-alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")} Осталось букв: ${remainingLetters}`);
+alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")}. Осталось букв: ${remainingLetters}`);
 alert(`Отлично! Было загадано слово ${randomWord}`);
 
 console.log(finalWord);
