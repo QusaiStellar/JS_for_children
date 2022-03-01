836af1abae1d58c7c0d694bf30429df24e886e86
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-01 15:48:57 +0300

    it works!

--- 21.js
+++ 21.js
@@ -2,12 +2,9 @@ function pickWord() {
    var words = ["Слава Украине!", "Жыве беларусь", "Птн пнх",];
    return words[Math.floor(Math.random() * words.length)];
 }
-var randomWord = pickWord();
-console.log(randomWord);
-
 function setupFinalWord(randomWord) {
    for (i = 0; i < randomWord.length; i++) {
-      setupFinalWord[i] = "_";
+      finalWord[i] = "_";
    }
    return;
 }
@@ -15,7 +12,7 @@ function showFunctionProgress(finalWord) {
    alert(`${finalWord.join(" ")} Осталось букв: ${remainingLetters}. Осталось попыток ${attempts}`);
 }
 function getGuess() {
-   prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+   return prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
 }
 function updateGameState(guess, randomWord, finalWord) {
    for (j = 0; j < randomWord.length; j++) {
@@ -25,45 +22,45 @@ function updateGameState(guess, randomWord, finalWord) {
             remainingLetters--;
          } else {
             alert(`Вы уже угадали букву: ${guess}`);
-            continue;
+            return;
          }
       }
    }
    attempts--;
 }
-
-console.log(randomWord);
-var finalWord = setupFinalWord(randomWord);
+function showFinalResult() {
+   alert(`Ваш окончательный результат: ${finalWord.join(" ")}.\nОсталось угадать ${remainingLetters} букв.\nБыло загадано слово: ${randomWord}`);
+}
+var randomWord = pickWord();
 var remainingLetters = randomWord.length;
-var attempts = randomWord.length;
-guessTheLetter: while (remainingLetters > 0) {
+var attempts = randomWord.length + 2;
+var finalWord = [];
+setupFinalWord(randomWord);
+console.log(finalWord);
+alert(`Добро пожаловать в игру Виселица!`);
+while (remainingLetters > 0) {
    showFunctionProgress(finalWord);
    var guess = getGuess();
+   console.log(guess);
    if (guess === null) {
       break;
    } else if (guess === "") {
       alert(`Пожалуйста, введите хоть что-то!`);
    } else if (guess.length > 1) {
       if (guess.toLowerCase() === randomWord.toLowerCase()) {
-         alert(`Вы ввели слово: ${guess.toLowerCase()} и это правильный ответ. Поздравяем!`);
+         alert(`Вы ввели слово: ${guess.toLowerCase()} и это правильный ответ.\nПоздравяем!`);
          break;
       } else {
          attempts--;
-         alert(`Вы ввели слово: ${guess} но оно неверно!! Осталось попыток: ${attempts}. Осталось букв: ${remainingLetters}`);
+         alert(`Вы ввели слово: ${guess} но оно неверно!!\nОсталось попыток: ${attempts}.\nОсталось букв: ${remainingLetters}`);
       }
    } else if (guess.length == 1) {
-      var correctGuesses = updateGameState(guess, word, finalWord);
+      var correctGuesses = updateGameState(guess, randomWord, finalWord);
+      console.log(finalWord);
    }
    if (attempts == false) {
       alert(`Увы, но вы исчерпали все свои попытки.`);
-      alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")}. Осталось букв: ${remainingLetters}`);
-      alert(`Отлично! Было загадано слово ${randomWord}`);
       break;
    }
-   console.log(guess);
 }
-
-alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")}. Осталось букв: ${remainingLetters}`);
-alert(`Отлично! Было загадано слово ${randomWord}`);
-
-console.log(finalWord);
+showFinalResult(finalWord, remainingLetters, randomWord);
