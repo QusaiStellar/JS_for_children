0b5a798c26c0f5e37a2bbaa69bdef96df8901181
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-05 18:31:16 +0300

    yeah

--- 24.js
+++ 24.js
@@ -1,22 +1,34 @@
 var leftOffset = 0;
 var topOffset = 0;
+var speedAnimationHead = 10;
+var popal = 0;
+var headingText = $(".heading").text();
+function stopAnimation(animationName) {
+   clearInterval(animationName);
+}
 var moveHeading = function () {
    $(".heading").offset({ left: leftOffset, top: topOffset });
    if (leftOffset < 200 && topOffset === 0) {
       leftOffset++;
-      console.log("left " + leftOffset);
-
    } else if (leftOffset === 200 && topOffset < 200) {
       topOffset++;
-      console.log("top " + topOffset);
-
    } else if (topOffset === 200 && leftOffset <= 200 && 0 < leftOffset) {
       leftOffset--;
-      console.log("left " + leftOffset);
    } else if (leftOffset === 0 && topOffset <= 200) {
       topOffset--;
-      console.log("top " + topOffset);
    }
 };
+var animation = setInterval(moveHeading, speedAnimationHead);
+$(".heading").click(function () {
+   if (popal < 2) {
+      stopAnimation(animation);
+      speedAnimationHead /= 2;
+      popal++;
+      animation = setInterval(moveHeading, speedAnimationHead);
+      $(".heading").text(headingText + " " + popal);
+   } else {
+      stopAnimation(animation);
+      $(".heading").text("You win!");
+   }
+});
 
-setInterval(moveHeading, 10);
