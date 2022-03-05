11dadf90a05074558969ba9cf63d38bf744591c1
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-05 23:35:16 +0300

    good

--- 27.js
+++ 27.js
@@ -9,44 +9,62 @@ var getDistance = function (event, target) {
 }
 function getDistanceHint(distance) {
    if (distance < 10) {
-      $('.tresure-map').css('background', 'radial-gradient(circle, rgba(255,0,0,1) 0%, rgba(255,0,0,1) 50%, rgba(254,221,0,0) 70%)');
+      $('.map').css('background', 'radial-gradient(circle, rgba(255,0,0,1) 0%, rgba(255,0,0,1) 40%, rgba(254,221,0,0) 55%)');
       return "Обожжешься!";
    } else if (distance < 20) {
-      $('.tresure-map').css('background', 'radial-gradient(circle, rgba(111,0,0,1) 0%, rgba(156,15,15,1) 50%, rgba(254,221,0,0) 70%)');
+      $('.map').css('background', 'radial-gradient(circle, rgba(111,0,0,1) 0%, rgba(156,15,15,1) 40%, rgba(254,221,0,0) 55%)');
       return "Очень горячо";
    } else if (distance < 40) {
-      $('.tresure-map').css('background', 'radial-gradient(circle, rgba(111,0,0,1) 0%, rgba(156,76,15,1) 50%, rgba(254,221,0,0) 70%)');
+      $('.map').css('background', 'radial-gradient(circle, rgba(111,0,0,1) 0%, rgba(156,76,15,1) 40%, rgba(254,221,0,0) 55%)');
       return "Горячо";
    } else if (distance < 80) {
-      $('.tresure-map').css('background', 'radial-gradient(circle, rgba(111,0,0,1) 0%, rgba(156,128,15,1) 50%, rgba(240,254,0,0) 70%)');
+      $('.map').css('background', 'radial-gradient(circle, rgba(111,0,0,1) 0%, rgba(156,128,15,1) 40%, rgba(240,254,0,0) 55%)');
       return "Тепло";
    } else if (distance < 160) {
-      $('.tresure-map').css('background', 'radial-gradient(circle, rgba(0,18,111,1) 0%, rgba(15,74,156,1) 30%, rgba(0,5,254,0) 70%)');
+      $('.map').css('background', 'radial-gradient(circle, rgba(0,18,111,1) 0%, rgba(15,74,156,1) 40%, rgba(0,5,254,0) 55%)');
       return "Холодно";
    } else if (distance < 320) {
-      $('.tresure-map').css('background', 'radial-gradient(circle, rgba(0,18,111,1) 0%, rgba(15,74,156,1) 50%, rgba(10,131,176,0) 70%)');
+      $('.map').css('background', 'radial-gradient(circle, rgba(0,18,111,1) 0%, rgba(15,74,156,1) 40%, rgba(10,131,176,0) 55%)');
       return "Очень холодно";
    } else {
       return "Замерзнешь!";
 
    }
 };
-var width = 400;
-var height = 400;
+var width = 800;
+var height = 500;
 var clicks = 0;
-var target = {
-   x: getRandomNumber(width),
-   y: getRandomNumber(height),
-};
+var remainedClicks = 5;
+var clicksText = $(".clicks").text();
+$(".clicks").text(clicksText + " " + remainedClicks);
+var target;
+function updateTarget() {
+   target = {
+      x: getRandomNumber(width),
+      y: getRandomNumber(height),
+   };
+}
+updateTarget();
 console.log(target);
-
 $(".tresure-map").click(function (event) {
    clicks++;
+   remainedClicks--;
+   $(".clicks").text(clicksText + " " + remainedClicks);
    var distance = getDistance(event, target);
    var distanceHint = getDistanceHint(distance);
    $(".distance").text(distanceHint);
    if (distance < 8) {
-      alert(`Клад найден! Сделано кликов ${clicks}`)
+      updateTarget();
+      console.log(target);
+      alert(`Клад найден! Сделано кликов ${clicks}`);
+
+   }
+   if (remainedClicks == 0) {
+      updateTarget();
+      console.log(target);
+      remainedClicks = 5;
+      alert("Ну всё, проиграл");
    }
 });
+
 
