e2f14c5c568f568c409cf2fcb85817b9ce79e1b9
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-08 12:23:58 +0300

    need to finish

--- 16.js
+++ 16.js
@@ -1,3 +1,48 @@
+var canvasHuman = document.getElementById('human');
+var ctxHuman = canvasHuman.getContext('2d');
+function drawHuman(x, y) {
+   ctxHuman.fillStyle = "Black";
+   ctxHuman.strokeStyle = "Black";
+   ctxHuman.lineWidth = 5;
+   ctxHuman.strokeRect(50 + x, 0 + y, 20, 20);
+   ctxHuman.beginPath();
+   ctxHuman.moveTo(60 + x, 20 + y);
+   ctxHuman.lineTo(60 + x, 80 + y); //тело
+   ctxHuman.moveTo(60 + x, 40 + y);
+   ctxHuman.lineTo(40 + x, 30 + y); //левая рука
+   ctxHuman.moveTo(60 + x, 40 + y);
+   ctxHuman.lineTo(80 + x, 30 + y); //права рука
+   ctxHuman.moveTo(60 + x, 80 + y);
+   ctxHuman.lineTo(30 + x, 120 + y); //левая нога
+   ctxHuman.moveTo(60 + x, 80 + y);
+   ctxHuman.lineTo(90 + x, 120 + y); //правая нога
+   ctxHuman.stroke();
+}
+drawHuman(100, 100);
+
+var canvasWord = document.getElementById('word');
+var ctxWord = canvasWord.getContext('2d');
+function drawWord(x, y) {
+   ctxHuman.fillStyle = "Black";
+   ctxHuman.strokeStyle = "Black";
+   ctxHuman.lineWidth = 5;
+   ctxHuman.strokeRect(50 + x, 0 + y, 20, 20);
+   ctxHuman.beginPath();
+   ctxHuman.moveTo(60 + x, 20 + y);
+   ctxHuman.lineTo(60 + x, 80 + y); //тело
+   ctxHuman.moveTo(60 + x, 40 + y);
+   ctxHuman.lineTo(40 + x, 30 + y); //левая рука
+   ctxHuman.moveTo(60 + x, 40 + y);
+   ctxHuman.lineTo(80 + x, 30 + y); //права рука
+   ctxHuman.moveTo(60 + x, 80 + y);
+   ctxHuman.lineTo(30 + x, 120 + y); //левая нога
+   ctxHuman.moveTo(60 + x, 80 + y);
+   ctxHuman.lineTo(90 + x, 120 + y); //правая нога
+   ctxHumanctx.stroke();
+}
+
+
+
 var words = ["богдан", "этосамое", "какего",];
 var randomWord = words[Math.floor(Math.random() * words.length)];
 var finalWord = [];
@@ -7,19 +52,19 @@ for (i = 0; i < randomWord.length; i++) {
 var remainingLetters = randomWord.length;
 var attempts = randomWord.length;
 guessTheLetter: while (remainingLetters > 0) {
-   alert(`${finalWord.join(" ")} Осталось букв: ${remainingLetters}. Осталось попыток ${attempts}`);
-   var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+   //alert(`${finalWord.join(" ")} Осталось букв: ${remainingLetters}. Осталось попыток ${attempts}`);
+   //var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
    if (guess === null) {
       break;
    } else if (guess === "") {
-      alert(`Пожалуйста, введите хоть что-то!`);
+      //alert(`Пожалуйста, введите хоть что-то!`);
    } else if (guess.length > 1) {
       if (guess.toLowerCase() === randomWord.toLowerCase()) {
-         alert(`Вы ввели слово: ${guess.toLowerCase()} и это правильный ответ. Поздравяем!`);
+         //alert(`Вы ввели слово: ${guess.toLowerCase()} и это правильный ответ. Поздравяем!`);
          break;
       } else {
          attempts--;
-         alert(`Вы ввели слово: ${guess} но оно неверно!! Осталось попыток: ${attempts}. Осталось букв: ${remainingLetters}`);
+         //alert(`Вы ввели слово: ${guess} но оно неверно!! Осталось попыток: ${attempts}. Осталось букв: ${remainingLetters}`);
       }
    } else if (guess.length == 1) {
       for (j = 0; j < randomWord.length; j++) {
@@ -28,7 +73,7 @@ guessTheLetter: while (remainingLetters > 0) {
                finalWord[j] = guess;
                remainingLetters--;
             } else {
-               alert(`Вы уже угадали букву: ${guess}`);
+               //alert(`Вы уже угадали букву: ${guess}`);
                continue guessTheLetter;
             }
          }
@@ -36,7 +81,7 @@ guessTheLetter: while (remainingLetters > 0) {
       attempts--;
    }
    if (attempts == false) {
-      alert(`Увы, но вы исчерпали все свои попытки.`);
+      //alert(`Увы, но вы исчерпали все свои попытки.`);
       break;
    }
    console.log(guess);
@@ -47,7 +92,6 @@ var ctx = canvas.getContext('2d');
 ctx.lineWidth = 5;
 ctx.strokeRect(0, 0, 50, 50);
 /*========================================================*/
-alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")}. Осталось букв: ${remainingLetters}`);
-alert(`Отлично! Было загадано слово ${randomWord}`);
-
+//alert(`Окончательный результат: ${finalWord.toLowerCase().join(" ")}. Осталось букв: ${remainingLetters}`);
+//alert(`Отлично! Было загадано слово ${randomWord}`);
 console.log(finalWord);
--- 31.js
+++ 31.js
@@ -2,6 +2,7 @@ const first = [1, 2, 3, 4];
 const second = [3, 4, 5, 6];
 
 function intersection(a, b) {
+   
    if (a[i] == b[i]) {
 
    }
