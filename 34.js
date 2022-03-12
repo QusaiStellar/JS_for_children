f302e22633b433e31ee6cc4c1bb94c69f69b5668
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-12 15:34:49 +0300

    add

---
+++ 34.js
@@ -0,0 +1,7 @@
+var canvasHtml = document.getElementById("snake");
+alert(canvasHtml.width)
+var canvas = document.getElementById("canvas");
+alert(canvas.height)
+var ctx = canvas.getContext("2d");
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
