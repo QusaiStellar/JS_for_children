1551adeaa587f9c431d04b8c8eda2a7676456c62
QusaiStellar <omgdobrik@gmail.com>
2022-03-13 20:10:55 +0300

    add

--- 34.js
+++ 34.js
@@ -1,7 +1,10 @@
-var canvasHtml = document.getElementById("snake");
-alert(canvasHtml.width)
-var canvas = document.getElementById("canvas");
-alert(canvas.height)
-var ctx = canvas.getContext("2d");
-var canvasWidth = canvas.width;
-var canvasHeight = canvas.height;
+(() => {
+   const canvas = document.getElementById("canvas");
+   const ctx = canvas.getContext("2d");
+
+   function init() {
+      canvas.width = document.getElementById('snake').offsetWidth;
+      canvas.height = document.getElementById('snake').offsetHeight;
+   }
+   init();
+})();
