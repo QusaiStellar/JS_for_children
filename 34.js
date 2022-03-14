f818cd8baf99657bbac455956e55fc767988606d
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-14 16:28:41 +0300

    snake

--- 34.js
+++ 34.js
@@ -7,4 +7,36 @@
       canvas.height = document.getElementById('snake').offsetHeight;
    }
    init();
+
+   const width = canvas.width;
+   const height = canvas.height;
+   const blockSize = 10;
+   const widthInBlocks = width / blockSize;
+   const heightInBlocks = height / blockSize;
+   const score = 0;
+
+   function drawBorder() {
+      ctx.fillStyle = "Grey";
+      ctx.fillRect(0, 0, width, blockSize);
+      ctx.fillRect(0, height - blockSize, width, blockSize);
+      ctx.fillRect(0, 0, blockSize, height);
+      ctx.fillRect(width - blockSize, 0, blockSize, height);
+
+   };
+
+   function drawScore() {
+      ctx.textBaseLine = "top";
+      ctx.fillText("Hellp", 20, 20);
+   }
+
+   function loop() {
+      canvas.width |= 0;
+      drawBorder();
+      drawScore();
+      requestAnimationFrame(loop);
+   }
+   loop();
+
+   window.addEventListener(`resize`, init);
+
 })();
